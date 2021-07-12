faa347ba0388fd621fb86e1465121636ad7875d8
RoqueBot <theapiworld@gmail.com>
2021-07-12 14:27:44 -0500

    V1.0

--- src/app/components/web/layouts/dashboard/maincontainer/maincontainer.component.ts
+++ src/app/components/web/layouts/dashboard/maincontainer/maincontainer.component.ts
@@ -7,17 +7,33 @@ import {FormControl, Validators} from "@angular/forms";
     styleUrls: ['../dashboard.component.scss']
 })
 export class MaincontainerComponent implements OnInit {
-    resultado = {};
+    resultadoFin: any = {}
+    key = {};
 
     constructor() {
     }
 
     ngOnInit() {
-        const array = [{value: 1, name: 'CampoUno'}, {value: 2, name: 'CampoDos'}, {
-            value: 3,
-            name: 'CampoTres'
-        }, {value: 4, name: 'CampoCuatro'}, {value: 5, name: 'CampoCinco'}, {value: 6, name: 'CampoSeis'},];
-        this.resultado = {CampoUno: 1, CampoDos: 2, CampoTres: 3, CampoCuatro: 4, CampoCinco: 5, CampoSeis: 6}
+        const array = [
+            {value: 1, name: 'CampoUno'},
+            {value: 2, name: 'CampoDos'},
+            {value: 3, name: 'CampoTres'}, {value: 4, name: 'CampoCuatro'}, {value: 5, name: 'CampoCinco'}, {
+                value: 6,
+                name: 'CampoSeis'
+            },];
 
+        for(let elemento of array){
+            this.resultadoFin[elemento.name] = elemento.value;
+        }
+
+        const arreglo = [];
+        for (let index in this.resultadoFin) {
+            arreglo.push({
+               value: this.resultadoFin[index],
+                name: index,
+            });
+        }
+        console.log(this.resultadoFin)
+        console.log(arreglo);
     }
 }
