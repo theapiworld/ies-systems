990769224ace9f3bfdb622b2fd96743fca7ea07d
RoqueBot <theapiworld@gmail.com>
2021-07-12 09:48:43 -0500

    Ejercicio 1

--- src/app/components/web/layouts/dashboard/maincontainer/maincontainer.component.ts
+++ src/app/components/web/layouts/dashboard/maincontainer/maincontainer.component.ts
@@ -7,10 +7,17 @@ import {FormControl, Validators} from "@angular/forms";
     styleUrls: ['../dashboard.component.scss']
 })
 export class MaincontainerComponent implements OnInit {
+    resultado = {};
 
     constructor() {
     }
 
     ngOnInit() {
+        const array = [{value: 1, name: 'CampoUno'}, {value: 2, name: 'CampoDos'}, {
+            value: 3,
+            name: 'CampoTres'
+        }, {value: 4, name: 'CampoCuatro'}, {value: 5, name: 'CampoCinco'}, {value: 6, name: 'CampoSeis'},];
+        this.resultado = {CampoUno: 1, CampoDos: 2, CampoTres: 3, CampoCuatro: 4, CampoCinco: 5, CampoSeis: 6}
+
     }
 }
